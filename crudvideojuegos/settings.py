3cca88c12afd626abff570e2860c1fe127beb265
chente-chester <vivalarazad@gmail.com>
2021-05-20 12:08:37 -0500

    algunas prubas con sqlite

--- crudvideojuegos/settings.py
+++ crudvideojuegos/settings.py
@@ -132,12 +132,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 
-#SMTP CONFIGURATION
-'''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'xxxxxxx@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxxxxx'
-'''
+#EMAIL_HOST_USER = 'xxxxxxx@gmail.com'
+#EMAIL_HOST_PASSWORD = 'xxxxxx'
+
+
